28ba0499e333e6f19ded662961fa578942a3f725
DmitrySolomatin <92448786+DmitrySolomatin@users.noreply.github.com>
2022-09-18 19:37:49 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+let submit = document.querySelector("#button-submit");
+submit.addEventListener("click", searchHandle);
+function displayText() {
+    let nameinput = document.querySelector("#name");
+    let name = name.value;
+    alert('Thank you, ${name}!')
+}
+
+displayText();
